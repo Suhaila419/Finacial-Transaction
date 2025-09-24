255bd75dc2432c704b9c5ccb7d6de85901011f86
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2025-09-24 12:40:03 +0000

    Add files via upload

---
+++ Query_Sql/SQLQuery2.sql
@@ -0,0 +1,31 @@
+use Financial;
+
+CREATE TABLE transactions_data(
+    id BIGINT,
+    date VARCHAR(MAX),
+    client_id BIGINT,
+    card_id BIGINT,
+    amount VARCHAR(MAX),
+    use_chip VARCHAR(MAX),
+    merchant_id VARCHAR(MAX),
+    merchant_city VARCHAR(MAX),
+    merchant_state VARCHAR(MAX),
+    zip VARCHAR(MAX),
+    mcc VARCHAR(MAX),
+    errors VARCHAR(MAX)
+);
+--DROP TABLE transactions_data;
+BULK INSERT transactions_data
+    FROM 'D:\Training_instant\Notebook\Excel\transactions_data.csv'
+    WITH (
+	    FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+		MAXERRORS = 1000
+    );
+
+
+SELECT *
+From Cards_cleaning as c 
+join User_cleaned as u on c.client_id = u.id
+join transactions_data as t on t.card_id = c.id;
